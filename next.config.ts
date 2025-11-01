897b68e35cf96c4a1cffc0fae9df58ed106dd18a
Miro Ostafinski <ostafinskim@gmail.com>
2025-11-01 15:48:03 +0000

    article editor complete

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import { dirname } from "node:path";
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   turbopack: {
     root: dirname(__filename),
